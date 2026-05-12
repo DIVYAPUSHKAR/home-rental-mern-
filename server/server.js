6fbaf509066f16f0cdbc3130a9bb5370c2185471
DIVYAPUSHKAR <pushkardivya535@gmail.com>
2026-05-12 16:22:21 +0530

    Added project screenshots

--- server/server.js
+++ server/server.js
@@ -31,14 +31,11 @@ app.use("/api/orders", orderRoutes);
 // 🔹 MongoDB Connection
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("✅ MongoDB Connected");
   } catch (err) {
     console.error("❌ MongoDB Connection Error:", err);
-    process.exit(1); // Exit process if DB connection fails
+    process.exit(1);
   }
 };
 
